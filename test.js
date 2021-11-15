db5a74bae2c37a5ef61620f2d1360ffe26c37edf
CmBSinka <43815006+CmBSinka@users.noreply.github.com>
2021-11-15 10:34:08 +0300

    666-19
    Очередной баг фикс
    Добавлена поломанная система баллов
    Потом допишу ее

---
+++ test.js
@@ -0,0 +1,10 @@
+let a = 20;
+let b = 30;
+engine.put("out", System.out);
+if (a===b) {
+    out.println('A = B');
+} else if (a > b) { 
+    out.println('A > B');
+} else {
+    out.println('A < B');
+}
